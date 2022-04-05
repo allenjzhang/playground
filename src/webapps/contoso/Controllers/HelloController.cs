80c50203eb42074c65d022fec0b17b943f39abc4
Allen Zhang <allenzhang@live.com>
2022-04-04 22:31:59 -0700

    Test contoso

---
+++ src/webapps/contoso/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace contoso.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET: api/<HelloController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "HELLO", "WORLD" };
+        }
+
+        // GET api/<HelloController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Hello, world! {id}";
+        }
+    }
+}
